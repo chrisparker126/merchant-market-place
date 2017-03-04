db6e8febc553ee5fc90b7af20df04fe98252ebbd
Chris.Parker <Chris.Parker@UK118.px.dps.local>
2017-03-04 16:41:00 +0000

    initial code

---
+++ src/main/java/marketplace/MerchantMarketPlaceApplication.java
@@ -0,0 +1,12 @@
+package marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchantMarketPlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerchantMarketPlaceApplication.class, args);
+	}
+}
---
+++ src/test/java/marketplace/MerchantMarketPlaceApplicationTests.java
@@ -0,0 +1,16 @@
+package marketplace;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MerchantMarketPlaceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
