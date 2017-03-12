75bdf0e0685a90f30e9f7909481dd3a55c8fe6d3
Chris.Parker <Chris.Parker@UK118.px.dps.local>
2017-03-12 15:31:49 +0000

    eclipse pom changes

--- src/main/java/marketplace/MerchantMarketPlaceApplication.java
+++ src/main/java/marketplace/MerchantMarketPlaceApplication.java
@@ -3,7 +3,7 @@ package marketplace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
-
+ 
 @SpringBootApplication
 @ImportResource("classpath:Beans.xml")
 public class MerchantMarketPlaceApplication {
